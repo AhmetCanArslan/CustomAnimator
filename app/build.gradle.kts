8ae61506a6fee67183e8d8ffae95e5a367dfdcf5
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2026-07-19 16:42:18 +0300

    chore: bump version to 2.0 (162)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.arslan.customanimator"
         minSdk = 24
         targetSdk = 36
-        versionCode = 161
-        versionName = "1.6"
+        versionCode = 162
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
